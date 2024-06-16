11cbb0d18df58b44d118c8ae0da259ee70f1bf4b
Yamileth Reveca <yamilethcaldera@gmail.com>
2024-06-16 19:04:40 -0400

    header-banner-items estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ItemsComponent } from './components/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BannerComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
